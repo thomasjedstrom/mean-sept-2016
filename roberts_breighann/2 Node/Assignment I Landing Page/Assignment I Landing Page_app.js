09ff216216f00d2e4bdd44d765d86ad25571ba6f
Breigh Ann Roberts <breighann21@gmail.com>
2016-09-14 15:19:57 -0500

    new committ

---
+++ roberts_breighann/2 Node/Assignment I Landing Page/Assignment I Landing Page_app.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+    if(request.url === '/') {
+        fs.readFile('Assignment I Landing Page_index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/ninjas") {
+         fs.readFile('Assignment I Landing Page_ninjas.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }else if (request.url === "/ninjas/new") {
+         fs.readFile('Assignment I Landing Page_dojos.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else {
+        response.end('The URL requested in not available');
+    }
+});
+server.listen(6789);
+console.log("Running in localhost at port 6789");
---
+++ roberts_breighann/2 Node/Assignment II Cars and Cats/app.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+    if(request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/cats") {
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }else if (request.url === "/cars/new") {
+        fs.readFile('views/new.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }else if(request.url === '/stylesheets/style.css'){
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'text/css'});
+            response.write(contents);
+            response.end();
+        })
+    }else if(request.url === '/images/cat.jpg'){
+        fs.readFile('./images/cat.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else if(request.url === '/images/car.jpg'){
+        fs.readFile('./images/car.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else {
+        response.end('The URL requested in not available');
+    }
+});
+server.listen(7077);
+console.log("Running in localhost at port 7077");
